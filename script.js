cd3f9105df3574cc84e13045501aa346fafcb6f0
drenimaimiri <drendedincajcoders@gmail.com>
2025-12-03 13:58:33 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+function setup() {
+  createCanvas(500,400);
+  background(186,145,20);
+  //pjata//
+  ellipse(250,200,450,350)
+  ellipse(250,200,400,300)
+  //pizza//
+  fill(255,183,0)
+  triangle(109,92,209,318,286,84)
+  //korja//
+  fill(224,127,9)
+  rect(94,79,206,31,117)
+  //proshuta//
+  fill(255,0,0)
+  ellipse(178,164,43,42)
+  ellipse(212,234,43,42)
+  ellipse(226,137,43,42)
+  
+
+  //rrushi 
+  fill(195,116,207)
+  ellipse(320,100,30,30)
+  ellipse(340,100,30,30)
+  ellipse(360,100,30,30)
+  ellipse(380,100,30,30)
+  ellipse(330,120,30,30)
+  ellipse(350,120,30,30)
+  ellipse(370,120,30,30)
+  ellipse(340,140,30,30)
+  ellipse(360,140,30,30)
+  ellipse(350,160,30,30)
+
+  //veze
+fill(255,255,255)
+ellipse(320,250,150)
+fill(255,255,25)
+ellipse(320,250,60,)
+
+}
